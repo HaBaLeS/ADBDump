bd1084bf7a962d6a7e01b9f9418cb2169c7e091f
falko <falko@lehmann-carpzov.de>
2018-03-18 19:59:40 +0100

    Added offline file support

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # ADBDump
 Command line tool written in Go to monitor the connection between android adb and and a android device.
 
-https://android.googlesource.com/platform/system/core/+/master/adb/OVERVIEW.TXT 
+This is a very early stage in development. It kinda works but do not expect too much. ***As always before you run anything with sudo, have a look a the code.***
 
 Install with 
   
-   go get -v  github.com/HaBaLeS/ADBDump
+    go get -v  -u  github.com/HaBaLeS/ADBDump
+
+if $GOPATH/bin is in your PATH run it with 
+
+    sudo ADBDump
+
+or 
+
+    sudo ADBDump -h for Options
+
+
--- main.go
+++ main.go
@@ -15,6 +15,7 @@ type Options struct {
 	captureInterface string
 	capturePort int
 	verbose bool
+	captureFile string
 }
 
 var options Options
@@ -27,14 +28,36 @@ func main() {
 	//FIXME introduce logger instead of writing directly anf commandline option to be silent
 
 
-	handle, err := pcap.OpenLive(options.captureInterface, 64000, true, pcap.BlockForever)
-	if err != nil {
-		color.Red("[e]\tCould not open device %s", options.captureInterface)
-		color.Red("\t%s",err)
+	//TODO dump files
 
+	//TODO Colorize Traffic --> Show short overview on packages
+
+	//TODO Verbose mode
+
+	//FIXME Pinning of 3rd party dependencies
+
+	var (
+		handle *pcap.Handle
+		err error
+	)
+
+	if options.captureFile != "" {
+		handle, err = pcap.OpenOffline(options.captureFile)
+		if err != nil {
+			color.Red("[e]\tCould not capture file %s", options.captureFile)
+			color.Red("\t%s",err)
+		}
 	} else {
-		color.HiGreen("[i]\tADBDump will start capturing on device: %s and port %d", options.captureInterface, options.capturePort)
+		handle, err = pcap.OpenLive(options.captureInterface, 64000, true, pcap.BlockForever)
+		if err != nil {
+			color.Red("[e]\tCould not open device %s", options.captureInterface)
+			color.Red("\t%s",err)
+		} else {
+			color.HiGreen("[i]\tADBDump will start capturing on device: %s and port %d", options.captureInterface, options.capturePort)
+		}
 	}
+
+
 
 	if handle != nil {
 		defer handle.Close()
@@ -68,9 +91,9 @@ func handlePayload(payload []byte, tcp *layers.TCP) {
 		if tcp.DstPort == 5037 { //Snip first 4 bytes
 			payload = payload[4:]
 
-			fmt.Printf("Server %d => ", len(payload))
+			fmt.Printf("[ADB]\t")
 		} else {
-			fmt.Printf("Client %d => ", len(payload))
+			fmt.Printf("[ANDR]\t")
 		}
 		max := 80
 		if len(payload) < max{
@@ -91,5 +114,6 @@ func init() {
 	flag.StringVar(&options.captureInterface, "d", "lo", "Device to capture on")
 	flag.IntVar(&options.capturePort, "p", 5037, "Port to capture on")
 	flag.BoolVar(&options.verbose, "v", false, "Log not only commands, also content of the communication")
+	flag.StringVar(&options.captureFile, "r", "" , "Read from file instead of ")
 
 }
