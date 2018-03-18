2531c7218c86e3c07962ad0b4ac301b50d6b8780
falko <falko@lehmann-carpzov.de>
2018-03-18 19:50:01 +0100

    First implementation
    Used lib pcap to capure packets and print them to console.
    No protocol specific highlighning

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Command line tool written in Go to monitor the connection between android adb and and a android device.
 
 https://android.googlesource.com/platform/system/core/+/master/adb/OVERVIEW.TXT 
+
+Install with 
+  
+   go get -v  github.com/HaBaLeS/ADBDump
--- main.go
+++ main.go
@@ -1,7 +1,95 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/fatih/color"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
+	"flag"
+)
 
-func main(){
-	fmt.Println("[x]Go ADBDump ")
+
+
+type Options struct {
+	captureInterface string
+	capturePort int
+	verbose bool
+}
+
+var options Options
+
+
+func main() {
+
+	flag.Parse()
+
+	//FIXME introduce logger instead of writing directly anf commandline option to be silent
+
+
+	handle, err := pcap.OpenLive(options.captureInterface, 64000, true, pcap.BlockForever)
+	if err != nil {
+		color.Red("[e]\tCould not open device %s", options.captureInterface)
+		color.Red("\t%s",err)
+
+	} else {
+		color.HiGreen("[i]\tADBDump will start capturing on device: %s and port %d", options.captureInterface, options.capturePort)
+	}
+
+	if handle != nil {
+		defer handle.Close()
+
+		filter := fmt.Sprintf("port %d", options.capturePort)
+		logD(fmt.Sprintf("Using \"%s\" as BPF Filter", filter))
+		e := handle.SetBPFFilter(filter)
+		if e != nil {
+			//FIXME Better error handling needed
+			panic(e)
+		}
+
+		// Loop through packets in file
+		packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+		for packet := range packetSource.Packets() {
+			tcpLayer := packet.Layer(layers.LayerTypeTCP)
+			if tcpLayer != nil {
+				tcp, _ := tcpLayer.(*layers.TCP)
+				payload := tcp.Payload
+				handlePayload(payload, tcp)
+			}
+		}
+
+	}
+
+}
+
+
+func handlePayload(payload []byte, tcp *layers.TCP) {
+	if len(payload) > 0 {
+		if tcp.DstPort == 5037 { //Snip first 4 bytes
+			payload = payload[4:]
+
+			fmt.Printf("Server %d => ", len(payload))
+		} else {
+			fmt.Printf("Client %d => ", len(payload))
+		}
+		max := 80
+		if len(payload) < max{
+			max = len(payload)
+		}
+		fmt.Println(string(payload[0:max]))
+	}
+}
+
+
+func logD(s string) {
+	color.HiGreen("[i]\t%s", s )
+}
+
+func init() {
+
+	options = Options{}
+	flag.StringVar(&options.captureInterface, "d", "lo", "Device to capture on")
+	flag.IntVar(&options.capturePort, "p", 5037, "Port to capture on")
+	flag.BoolVar(&options.verbose, "v", false, "Log not only commands, also content of the communication")
+
 }
